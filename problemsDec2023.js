83e6190ab882cc6147060114f76c4b6907c628f9
Benny Zheng <benny@davidenergy.com>
2023-12-11 15:52:22 -0500

    added 2 more older dailies

--- problemsDec2023.js
+++ problemsDec2023.js
@@ -11,3 +11,33 @@ var findSpecialInteger = function (arr) {
     }
   }
 };
+
+// https://leetcode.com/problems/transpose-matrix/?envType=daily-question&envId=2023-12-11
+var transpose = function (matrix) {
+  const length = matrix.length;
+  const width = matrix[0].length;
+  const result = [];
+  for (let i = 0; i < width; i++) {
+    result.push([]);
+    for (let j = 0; j < length; j++) {
+      result[i].push(matrix[j][i]);
+    }
+  }
+  return result;
+};
+
+// https://leetcode.com/problems/construct-string-from-binary-tree/?envType=daily-question&envId=2023-12-11
+var tree2str = function (root) {
+  let left = "";
+  let right = "";
+  if (root.left) {
+    left = `(${tree2str(root.left)})`;
+  }
+  if (root.right) {
+    right = `(${tree2str(root.right)})`;
+    if (!left) {
+      left = "()";
+    }
+  }
+  return `${root.val}${left}${right}`;
+};
