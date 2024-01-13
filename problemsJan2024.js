12e3442b099ae603f94c430e7c0c5086a321fb1e
Benny Zheng <benny@davidenergy.com>
2024-01-13 14:41:53 -0500

    few more problems and some partials

--- problemsJan2024.js
+++ problemsJan2024.js
@@ -122,3 +122,178 @@ var findContentChildren = function (g, s) {
   }
   return count;
 };
+
+// https://leetcode.com/problems/range-sum-of-bst/?envType=daily-question&envId=2024-01-03
+var rangeSumBST = function (root, low, high) {
+  let sum = 0;
+  if (root.val >= low && root.val <= high) {
+    sum += root.val;
+  }
+  if (root.left && root.val > low) {
+    sum += rangeSumBST(root.left, low, high);
+  }
+  if (root.right && root.val < high) {
+    sum += rangeSumBST(root.right, low, high);
+  }
+  return sum;
+};
+
+// https://leetcode.com/problems/arithmetic-slices/
+var numberOfArithmeticSlices = function (nums) {
+  if (nums.length < 3) return 0;
+  let count = 0;
+  let pattern = nums[1] - nums[0];
+  for (let i = 0; i < nums.length - 2; i++) {
+    let j = 1;
+    if (nums[i + 2] - nums[i + 1] == pattern) {
+      count++;
+      while (nums[i + 2 + j] - nums[i + 1 + j] == pattern) {
+        count++;
+        j++;
+      }
+    } else {
+      pattern = nums[i + 2] - nums[i + 1];
+    }
+  }
+  return count;
+};
+
+// https://leetcode.com/problems/leaf-similar-trees/submissions/?envType=daily-question&envId=2024-01-08
+var leafSimilar = function (root1, root2) {
+  const treeTraverser = (node) => {
+    let result = [];
+    if (!node.left && !node.right) {
+      return [node.val];
+    }
+    if (node.left) {
+      result.push(...treeTraverser(node.left));
+    }
+    if (node.right) {
+      result.push(...treeTraverser(node.right));
+    }
+    return result;
+  };
+  let r1 = treeTraverser(root1);
+  let r2 = treeTraverser(root2);
+  if (r1.length != r2.length) return false;
+  for (let i = 0; i < r1.length; i++) {
+    if (r1[i] != r2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// https://leetcode.com/problems/island-perimeter/submissions/
+var islandPerimeter = function (grid) {
+  let result = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] == 1) {
+        let value = 4;
+        if (grid[i + 1] && grid[i + 1][j] == 1) {
+          value--;
+        }
+        if (grid[i - 1] && grid[i - 1][j] == 1) {
+          value--;
+        }
+        if (grid[i][j + 1] == 1) {
+          value--;
+        }
+        if (grid[i][j - 1] == 1) {
+          value--;
+        }
+        result += value;
+      }
+    }
+  }
+  return result;
+};
+
+var islandPerimeter2 = function (grid) {
+  let result = 0;
+  let gridMap = grid.map((row) =>
+    row.reduce((acc, curr) => {
+      if (curr == 1) {
+        acc++;
+      }
+      return acc;
+    }, 0)
+  );
+  for (let i = 0; i < gridMap.length; i++) {
+    let value = gridMap[i] * 2 + 2;
+    if (gridMap[i - 1]) {
+      // TODO: adjust these differences to account for misaligned islands
+      let difference = Math.abs(grid[i].indexOf(1) - grid[i - 1].indexOf(1));
+      value -= Math.min(gridMap[i], gridMap[i - 1]) - difference;
+    }
+    if (gridMap[i + 1]) {
+      let difference = Math.abs(grid[i].indexOf(1) - grid[i + 1].indexOf(1));
+      value -= Math.min(gridMap[i], gridMap[i + 1]) - difference;
+    }
+    result += value;
+  }
+  return result;
+};
+
+// islandPerimeter([
+//   [0, 1, 0, 0],
+//   [1, 1, 1, 0],
+//   [0, 1, 0, 0],
+//   [1, 1, 0, 0],
+// ]);
+
+// https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/?envType=daily-question&envId=2024-01-10
+var maxAncestorDiff = function (root) {};
+
+var minMaxNode = function (root) {
+  if (!root.left && !root.right) {
+    return [root.val, root.val];
+  }
+  let min = root.val;
+  let max = root.val;
+  if (root.left) {
+    let left = minMaxNode(root.left);
+    min = Math.min(min, left[0]);
+    max = Math.max(max, left[1]);
+  }
+  if (root.right) {
+    let right = minMaxNode(root.right);
+    min = Math.min(min, right[0]);
+    max = Math.max(max, right[1]);
+  }
+  let result =
+    Math.abs(root.val - min) > Math.abs(max - root.val)
+      ? Math.abs(root.val - min)
+      : Math.abs(max - root.val);
+  return result;
+};
+
+var traverseTree = function (root, max = 0) {
+  if (!root.left && !root.right) {
+    return root.val;
+  }
+  if (root.left) {
+    result.push(...traverseTree(root.left));
+  }
+  if (root.right) {
+    result.push(...traverseTree(root.right));
+  }
+  return max;
+};
+
+// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/?envType=daily-question&envId=2024-01-13
+var minSteps = function (s, t) {
+  let steps = s.length;
+  let letters = {};
+  for (const letter of s) {
+    letters[letter] = letters[letter] ? letters[letter] + 1 : 1;
+  }
+  for (const letter of t) {
+    if (letters[letter] && letters[letter] > 0) {
+      letters[letter]--;
+      steps--;
+    }
+  }
+  return steps;
+};
