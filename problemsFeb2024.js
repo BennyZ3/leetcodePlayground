ac1daab0d89e2cf228c7ae6ca1db4cf6acf0e11e
Benny Zheng <benny@davidenergy.com>
2024-02-21 08:46:53 -0500

    added 10 more problems

--- problemsFeb2024.js
+++ problemsFeb2024.js
@@ -482,3 +482,300 @@ var numberToWords = function (num) {
   // remove trailing space
   return result.trim();
 };
+
+// https://leetcode.com/problems/integer-to-roman/
+var intToRoman = function (num) {
+  let numerals = {
+    1000: "M",
+    500: "D",
+    100: "C",
+    50: "L",
+    10: "X",
+    5: "V",
+    1: "I",
+  };
+  let numeralEntries = Object.entries(numerals);
+  let result = "";
+  for (let i = numeralEntries.length - 1; i >= 0; i--) {
+    let val = Number(numeralEntries[i][0]);
+    while (num >= val) {
+      result += numeralEntries[i][1];
+      num -= val;
+    }
+    if (`${num}`[0] == "9" && `${val}`[0] == "1" && num > val * 0.75) {
+      result += numeralEntries[i - 2][1] + numeralEntries[i][1];
+      num -= val - Number(numeralEntries[i - 2][0]);
+    }
+    if (`${num}`[0] == "4" && `${val}`[0] == "5" && num > val * 0.75) {
+      result += numeralEntries[i - 1][1] + numeralEntries[i][1];
+      num -= val - Number(numeralEntries[i - 1][0]);
+    }
+  }
+  return result;
+};
+
+// https://leetcode.com/problems/text-justification/
+var fullJustify = function (words, maxWidth) {
+  let output = [];
+  let row = [words[0]];
+  let count = words[0].length;
+  for (let i = 1; i <= words.length; i++) {
+    if (i == words.length) {
+      output.push(row.join(" ") + " ".repeat(maxWidth - count));
+    } else if (count + words[i].length >= maxWidth) {
+      let gap = (maxWidth - count) / (row.length - 1 || 1);
+      if (gap < 0) {
+        gap = 0;
+      }
+      if (gap % 1 > 0) {
+        let remainder = Math.round((gap % 1) * (row.length - 1));
+        for (let i = 0; i < remainder; i++) {
+          row[i] = row[i] + " ";
+        }
+      }
+      if (row.length == 1) {
+        output.push(row[0] + " ".repeat(gap));
+      } else {
+        output.push(row.join(" ".repeat(Math.floor(1 + gap))));
+      }
+      row = [words[i]];
+      count = words[i].length;
+    } else {
+      count += count == 0 ? words[i].length : words[i].length + 1;
+      row.push(words[i]);
+    }
+  }
+  return output;
+};
+
+// biweekly 124
+var lastNonEmptyString = function (s) {
+  let letters = {};
+  let max = 0;
+  for (let letter of s) {
+    letters[letter] = letters[letter] + 1 || 1;
+    if (letters[letter] > max) {
+      max = letters[letter];
+    }
+  }
+  max--;
+  s = s.split("");
+  while (max > 0) {
+    for (let letter in letters) {
+      let found = s.findIndex((element) => element === letter);
+      if (found >= 0) {
+        s.splice(found, 1);
+      }
+    }
+    max--;
+  }
+  return s.join("");
+};
+
+var maxOperations = function (nums) {
+  let scores = [
+    nums[0] + nums[1],
+    nums[0] + nums[nums.length - 1],
+    nums[nums.length - 1] + nums[nums.length - 2],
+  ];
+  let maxCount = 0;
+  for (let score of scores) {
+    let count = scoreTraverser(nums, score);
+    if (count > maxCount) {
+      maxCount = count;
+    }
+  }
+  return maxCount;
+};
+
+let scoreTraverser = function (nums, score) {
+  let max = 0;
+  if (nums.length <= 1) {
+    return 0;
+  }
+  if (nums[0] + nums[1] == score) {
+    let result = 1 + scoreTraverser(nums.slice(2), score);
+    if (result > max) {
+      max = result;
+    }
+  }
+  if (nums[0] + nums[nums.length - 1] == score) {
+    let result = 1 + scoreTraverser(nums.slice(1, -1), score);
+    if (result > max) {
+      max = result;
+    }
+  }
+  if (nums[nums.length - 1] + nums[nums.length - 2] == score) {
+    let result = 1 + scoreTraverser(nums.slice(0, -2), score);
+    if (result > max) {
+      max = result;
+    }
+  }
+  return max;
+};
+
+var maxSelectedElements = function (nums) {
+  nums.sort((a, b) => a - b);
+  let numList = {};
+  for (let num of nums) {
+    numList[num] = numList[num] + 1 || 1;
+  }
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (numList[nums[i]] > 2) {
+      numList[nums[i]] = numList[nums[i]] - 1;
+      nums.splice(i, 1);
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= nums[i - 1]) {
+      nums[i] = nums[i] + 1;
+    } else if (nums[i] == nums[i + 1] - 2) {
+      nums[i] = nums[i] + 1;
+    }
+  }
+  let set = [...new Set(nums)];
+  let currentSequence = 1;
+  let longest = 1;
+  for (let i = 0; i < set.length; i++) {
+    if (set[i] + 1 == set[i + 1]) {
+      currentSequence++;
+      if (currentSequence > longest) {
+        longest = currentSequence;
+      }
+    } else {
+      currentSequence = 1;
+    }
+  }
+  return longest;
+};
+
+// https://leetcode.com/problems/furthest-building-you-can-reach/description/?envType=daily-question&envId=2024-02-17
+var furthestBuilding = function (heights, bricks, ladders) {
+  return buildingTraversal(heights, bricks, ladders, 0);
+};
+
+let buildingTraversal = function (heights, bricks, ladders, index) {
+  if (index == heights.length - 1) {
+    return index;
+  }
+  let highest = index;
+  if (heights[index] >= heights[index + 1]) {
+    let result = buildingTraversal(heights, bricks, ladders, index + 1);
+    if (result > highest) {
+      highest = result;
+    }
+  } else {
+    if (bricks > 0 && heights[index + 1] - heights[index] <= bricks) {
+      let result = buildingTraversal(
+        heights,
+        bricks - (heights[index + 1] - heights[index]),
+        ladders,
+        index + 1
+      );
+      if (result > highest) {
+        highest = result;
+      }
+    }
+    if (ladders > 0) {
+      let result = buildingTraversal(heights, bricks, ladders - 1, index + 1);
+      if (result > highest) {
+        highest = result;
+      }
+    }
+  }
+  return highest;
+};
+
+console.log(furthestBuilding([4, 2, 7, 6, 9, 14, 12], 5, 1));
+console.log(furthestBuilding([4, 12, 2, 7, 3, 18, 20, 3, 19], 10, 2));
+console.log(furthestBuilding([14, 3, 19, 3], 17, 0));
+console.log(furthestBuilding([17, 16, 5, 10, 10, 14, 7], 74, 6));
+console.log(furthestBuilding([1, 5, 1, 2, 3, 4, 10000], 4, 1));
+
+// https://leetcode.com/problems/power-of-two/?envType=daily-question&envId=2024-02-19
+var isPowerOfTwo = function (n) {
+  if (n < 0) {
+    return false;
+  }
+  return (
+    n
+      .toString(2)
+      .split("")
+      .filter((char) => char == "1").length == 1
+  );
+};
+var isPowerOfTwo = function (n) {
+  if (n < 0) {
+    return false;
+  }
+  return n.toString(2).replaceAll("0", "") == "1";
+};
+
+// https://leetcode.com/problems/power-of-three/
+var isPowerOfThree = function (n) {
+  if (n < 1) {
+    return false;
+  }
+  return n.toString(3).replaceAll("0", "") == "1";
+};
+
+// https://leetcode.com/problems/power-of-four/
+var isPowerOfFour = function (n) {
+  if (n < 1) {
+    return false;
+  }
+  return n.toString(4).replaceAll("0", "") == "1";
+};
+
+// https://leetcode.com/problems/meeting-rooms-iii/
+var mostBooked = function (n, meetings) {
+  meetings.sort((a, b) => a[0] - b[0]);
+  let numOfMeetings = new Array(n).fill(0);
+  let currentEndTime = new Array(n).fill(0);
+  for (let meeting of meetings) {
+    let opening = currentEndTime.findIndex(
+      (openTime) => openTime <= meeting[0]
+    );
+    if (opening != -1) {
+      numOfMeetings[opening]++;
+      currentEndTime[opening] = meeting[1];
+    } else {
+      let earliestOpening = Math.min(...currentEndTime);
+      opening = currentEndTime.findIndex(
+        (openTime) => openTime == earliestOpening
+      );
+      numOfMeetings[opening]++;
+      currentEndTime[opening] += meeting[1] - meeting[0];
+    }
+  }
+  return numOfMeetings.findIndex(
+    (count) => count == Math.max(...numOfMeetings)
+  );
+};
+
+// https://leetcode.com/problems/bitwise-and-of-numbers-range/?envType=daily-question&envId=2024-02-21
+var rangeBitwiseAnd = function (left, right) {
+  left = left.toString(2);
+  right = right.toString(2);
+  let combo = "";
+  if (left.length > right.length) {
+    right = "0".repeat(left.length - right.length) + right;
+  }
+  if (left.length < right.length) {
+    right = "0".repeat(right.length - left.length) + left;
+  }
+  let flag = true;
+  for (let i = 0; i < left.length; i++) {
+    if (flag) {
+      if (left[i] == right[i]) {
+        combo += left[i];
+      } else {
+        combo += "0";
+        flag = false;
+      }
+    } else {
+      combo += "0";
+    }
+  }
+  return parseInt(combo, 2);
+};
